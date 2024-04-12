0fe39fc8788d04818ebb71504f612921b3847a3f
unknown <ironkatzl@gmail.com>
2024-04-12 21:33:55 +0200

    implement first controller actions and inmemeory storage

--- RunescapeGroupFinder.Backend/Controllers/BossAssignmentController.cs
+++ RunescapeGroupFinder.Backend/Controllers/BossAssignmentController.cs
@@ -1,5 +1,39 @@
-﻿namespace RunescapeGroupFinder.Backend.Controllers;
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using RunescapeGroupFinder.Backend.Storage;
+using RunescapeGroupFinder.Models;
 
-public class BossAssignmentController {
+namespace RunescapeGroupFinder.Backend.Controllers;
+
+[ApiController]
+[Route("[controller]/v1")]
+public class BossAssignmentController(IStorageRegistry storageRegistry) : ControllerBase {
+
+  [HttpGet("GetBosses")]
+  public ActionResult<List<Boss>> GetBosses() {
+
+    
+    var bosses = new List<Boss>() {
+      Bosses.Solak,
+      Bosses.BeastMaster,
+      Bosses.Aod,
+      Bosses.Rago3Plus,
+      Bosses.RagoDuo
+    };
+    
+    return bosses;
+  }
   
+  [HttpPost("RegisterPlayer/{bossName}")]
+  public async  Task<ActionResult> RegisterPlayer(string bossName, [FromBody] Player player) {
+
+    var boss = Bosses.AllBosses.Find(x => x.BossName == bossName);
+
+    if (boss == null)
+      return NotFound($"A boss with the name {bossName} was not found");
+
+    await storageRegistry.AssignPlayerToBoss(boss, player);
+
+    return Ok();
+  }
 }
--- RunescapeGroupFinder.Backend/Storage/IStorageRegistry.cs
+++ RunescapeGroupFinder.Backend/Storage/IStorageRegistry.cs
@@ -1,5 +1,28 @@
-﻿namespace RunescapeGroupFinder.Backend.Storage;
+﻿using System.Collections.Concurrent;
+using RunescapeGroupFinder.Models;
 
-public interface IBossRegistry {
-  
+namespace RunescapeGroupFinder.Backend.Storage;
+
+public interface IStorageRegistry {
+  Task AssignPlayerToBoss(Boss boss, Player player);
+  Task<List<Player>> GetSearchingPlayersForBoss(Boss boss);
+}
+
+public class InMemoryRegistry(ConcurrentDictionary<Boss, List<Player>> bossRegistry) : IStorageRegistry {
+  public Task AssignPlayerToBoss(Boss boss, Player player) {
+    
+    if (bossRegistry.TryGetValue(boss, out var players)) {
+      players.Add(player);
+      return Task.CompletedTask;
+    }
+    
+    var newPlayerList = new List<Player> { player };
+    bossRegistry.TryAdd(boss, newPlayerList);
+
+    return Task.CompletedTask;
+  }
+
+  public Task<List<Player>> GetSearchingPlayersForBoss(Boss boss) {
+    return Task.FromResult(bossRegistry.TryGetValue(boss, out var players) ? players : new List<Player>());
+  }
 }
